1f639cca3b2cc87436b33791ce048df0fc93ac25
FernandoGomes <fernandogomes90@gmail.com>
2016-01-21 03:08:10 +0000

    forgot to add date, remove console.log

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ var page = 0;
     xhr.onload = function(e){
       if (this.status == 200){
        var news = JSON.parse(this.response);
-       console.log(news);
        printjson(news);
     }else {
       console.log("error");
@@ -26,8 +25,10 @@ var page = 0;
       var story="";
       var i;
       for(i = 0; i < arr.hits.length; i++) {
-
-        story += '<a href="'+ arr.hits[i].url +'" class="list-group-item col-md-8 col-md-offset-2"><h4 class="list-group-item-heading">'+ arr.hits[i].title +'</h4><p class="list-group-item-text row"><span class="col-md-2">'+ arr.hits[i].points +' points</span><span class="col-md-2">by '+ arr.hits[i].author +'</span><span class="col-md-2">'+ arr.hits[i].num_comments +'comments</span></p></a>';
+        var da = arr.hits[i].created_at;
+        var date = new Date(da);
+        var finalDate = date.getFullYear() + "-" + date.getMonth()+1 + "-" + date.getDate() ;
+        story += '<a href="'+ arr.hits[i].url +'" class="list-group-item col-md-8 col-md-offset-2"><h4 class="list-group-item-heading">'+ arr.hits[i].title +'</h4><p class="list-group-item-text row"><span class="col-md-2">'+ arr.hits[i].points +' points</span><span class="col-md-2">by '+ arr.hits[i].author +'</span><span class="col-md-2">'+ arr.hits[i].num_comments +'comments</span><span class="col-md-3">'+ finalDate +'</span></p></a>';
 
     }
     document.getElementById('list').innerHTML += story;
